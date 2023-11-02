7c553f48d52e2771537014e6fdcb25f6ad6135fb
Cedric Segura <cedricsegurafr@gmail.com>
2023-11-02 16:27:36 +0100

    SetUp path common and user dto

--- apps/users/dist/dto/update-user.dto.d.ts
+++ apps/users/dist/dto/update-user.dto.d.ts
@@ -1,5 +1,7 @@
-import { CreateUserDto } from './create-user.dto';
-declare const UpdateUserDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateUserDto>>;
-export declare class UpdateUserDto extends UpdateUserDto_base {
+export declare class UpdateUserDto {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
 }
-export {};
