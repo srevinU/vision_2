42e4eb722ba8eb3cc494c62cb5f7ac61318ede78
Cedric Segura <cedricsegurafr@gmail.com>
2023-11-14 11:36:13 +0100

    User microservice cleanUp

--- apps/users/src/users.controller.ts
+++ apps/users/src/users.controller.ts
@@ -1,38 +1,15 @@
-import {
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-  Controller,
-} from '@nestjs/common';
+import { Get, Param, Delete, Controller, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { AuthGuard } from '@app/common/guards/auth.guard';
 
 @Controller('api/users')
+@UseGuards(AuthGuard)
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto): Promise<any> {
-    return this.usersService.create(createUserDto);
-  }
-
-  @Get()
-  findAll(): Promise<any> {
-    return this.usersService.findAll();
-  }
-
   @Get(':email')
   findOne(@Param('email') email: string): Promise<any> {
-    return this.usersService.findOne(email);
-  }
-
-  @Patch()
-  update(@Body() updateUserDto: UpdateUserDto): Promise<any> {
-    return this.usersService.update(updateUserDto);
+    return this.usersService.findOneByEmail(email);
   }
 
   @Delete(':id')
--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,30 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '@app/common/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly Prisma: PrismaService) {}
 
-  async create(createUserDto: CreateUserDto) {
-    return await this.Prisma.user.create({
-      data: createUserDto,
-    });
-  }
-
-  async findAll() {
-    return await this.Prisma.user.findMany({
-      orderBy: { createdAt: 'desc' },
-      select: {
-        firstName: true,
-        lastName: true,
-        email: true,
-      },
-    });
-  }
-
-  async findOne(email: string) {
+  async findOneByEmail(email: string) {
     return await this.Prisma.user.findUnique({
       where: {
         email: email,
@@ -34,16 +15,6 @@ export class UsersService {
         lastName: true,
         email: true,
       },
-    });
-  }
-
-  async update(updateUserDto: UpdateUserDto) {
-    return await this.Prisma.user.upsert({
-      where: {
-        id: updateUserDto.id,
-      },
-      update: updateUserDto,
-      create: updateUserDto,
     });
   }
 
