53325463083a855c354f972475d190cec4662bb8
Cedric Segura <cedricsegurafr@gmail.com>
2023-11-21 15:47:27 +0100

    cookie parser added

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { env } from 'process';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+  app.use(cookieParser());
   await app.listen(env.PORT);
   console.log(`${env.APP_NAME} application listening on port ${env.PORT}`);
 }
--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { UsersModule } from './users.module';
 import { env } from 'process';
-// import { RolesGuard } from '@app/common/guards/roles.guard';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(UsersModule);
+  app.use(cookieParser());
   await app.listen(env.PORT);
-  // app.useGlobalGuards(new RolesGuard());
   console.log(`${env.APP_NAME} application listening on port ${env.PORT}`);
 }
 bootstrap();
