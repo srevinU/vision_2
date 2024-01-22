3c07d69ad7c9a3b5cd6db11d8bcf929b9865b7ac
Cedric Segura <cedricsegurafr@gmail.com>
2024-01-22 15:35:34 +0100

    jenkins file test behaviour

---
+++ jenkins/script.groovy
@@ -0,0 +1,11 @@
+def buildApp() {
+    echo 'Building the application... ${VARIABLE_TEST}'
+}
+
+def testApp() {
+    echo 'Testing the application...'
+}
+
+def deployApp() {
+    echo 'Deploying the application...'
+}
