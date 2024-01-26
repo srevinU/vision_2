dd9fab9820255cd0a16339422c076109fad354ec
Cedric Segura <cedricsegurafr@gmail.com>
2024-01-26 15:57:59 +0100

    more space for jenkins container & groovy init update

--- jenkins/staging/init.staging.groovy
+++ jenkins/staging/init.staging.groovy
@@ -4,7 +4,10 @@ def init() {
     sh "apt upgrade -y"
     sh "apt install make" 
     sh "apt install docker -y"
-    sh "apt install docker-compose -y" 
+    // sh "apt install docker-compose -y"
+    sh "curl -L 'https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep '\"tag_name\":' | sed -E 's/.*\"([^\"]+)\".*/\1/')/docker-compose-$(uname -s)-$(uname -m)' -o /usr/local/bin/docker-compose"
+    sh "chmod +x /usr/local/bin/docker-compose"
+    sh "touch key"
 }
 
 return this
