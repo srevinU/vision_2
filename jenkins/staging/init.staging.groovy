63acdb3b358b86c4ca3cbcd3b66a489fd0acdeeb
Cedric Segura <cedricsegurafr@gmail.com>
2024-01-26 20:09:03 +0100

    docker file created in order to install dependancies

--- jenkins/staging/init.staging.groovy
+++ jenkins/staging/init.staging.groovy
@@ -1,13 +1,5 @@
 def init() {
     echo 'Init...'
-    sh "apt update"
-    sh "apt upgrade -y"
-    sh "apt install make" 
-    sh "apt install docker -y"
-    // sh "apt install docker-compose -y"
-    sh "curl -L https://github.com/docker/compose/releases/download/v2.24.3/docker-compose-Linux-aarch64 -o /usr/local/bin/docker-compose"
-    sh "chmod +x /usr/local/bin/docker-compose"
-    sh "touch key"
 }
 
 return this
