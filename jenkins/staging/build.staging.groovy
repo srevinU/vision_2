edf8e923c17c7a0d29e8e481fa0e3cb38af516c6
Cedric Segura <cedricsegurafr@gmail.com>
2024-01-27 19:01:08 +0100

    sudo added to trigger docker

--- jenkins/staging/build.staging.groovy
+++ jenkins/staging/build.staging.groovy
@@ -1,6 +1,6 @@
 def buildApp() {
     sh "npm i"
-    sh "service docker start"
+    sh "sudo service docker start"
     // sh "STAGE_NAME=staging docker-compose -p 'vision-staging' up"
     // sh "make start-migration"
     // sh "make start-sedder"
